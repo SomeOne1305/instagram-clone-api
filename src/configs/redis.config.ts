bd71fda7b99fa465527b6c32736c7b41afd5cb43
SomeOne1305 <axmadxolmuminov2007@gmail.com>
2024-07-06 23:06:30 +0500

    redis

--- src/configs/redis.config.ts
+++ src/configs/redis.config.ts
@@ -3,10 +3,11 @@ import { RedisClientOptions } from 'redis';
 export default registerAs(
   'redisConfig',
   (): RedisClientOptions => ({
-    password: 'A!a2h3m4a5d@',
-    socket: {
-      host: process.env.REDIS_HOST,
-      port: +process.env.REDIS_PORT,
-    },
+    // password: 'A!a2h3m4a5d@',
+    // socket: {
+    //   host: process.env.REDIS_HOST,
+    //   port: +process.env.REDIS_PORT,
+    // },
+    url: 'redis://default:AanfAAIncDEyODBjOTM0YmZjMDE0NDE3YTkwY2JkNjNjNDA2YWE4ZnAxNDM0ODc@immense-baboon-43487.upstash.io:6379',
   }),
 );
