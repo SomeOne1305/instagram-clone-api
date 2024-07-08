5691d39136377746fea3ce9c78c7c110e101089b
SomeOne1305 <axmadxolmuminov2007@gmail.com>
2024-07-08 12:41:05 +0500

    edit url

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -2,5 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => [
   process.env.CLIENT_URL,
+  'https://insta-clone-application.vercel.app/',
+  'https://insta-clone-application.vercel.app',
   'http://localhost:5173',
 ]);
